ed8fde8bc248c288515db427ee64e5b50b04d6ee
Manvi Gupta <manvigupta226@gmail.com>
2023-02-05 10:53:51 +0530

    dev:get & put request

---
+++ main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app= FastAPI()
+class Item(BaseModel):
+    id:int
+    name:str 
+    description:str 
+    price:int 
+    on_offer:bool
+
+@app.get('/')
+def index():
+    return {"message":"Hello World"}
+
+
+@app.put('/item/{item_id}')
+def update_item(item_id:int, item:Item):
+    return {'name': item.name,
+    'description': item.description,
+    'price':item.price,
+    'on_offer':item.on_offer
+    }
+ 
